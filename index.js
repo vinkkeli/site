1e3a2760300ad548e0e88e57fe8577642fd6aae3
Mikko Vesikkala <mikko.vesikkala@reaktor.fi>
2015-12-11 17:54:36 +0200

    npm versions, concurrency and assets path

--- index.js
+++ index.js
@@ -5,8 +5,17 @@ var less  = require('metalsmith-less')
 var permalinks  = require('metalsmith-permalinks')
 var cleanCSS = require('metalsmith-clean-css')
 var fingerprint = require('metalsmith-fingerprint-ignore')
+var define = require('metalsmith-define')
+
+var production = (process.env || 'dev') === 'production'
+var assetsPath = production ? '' : '/build/'
 
 Metalsmith(__dirname)
+  .concurrency(50)
+  .use(define({
+    production: true,
+    assetsPath: assetsPath
+  }))
   .use(markdown())
   .use(less({pattern: 'styles/main.less'}))
   .use(cleanCSS({
