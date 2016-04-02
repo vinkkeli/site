f534a4b0d6725d4895547794be3c67f429228441
Mikko Vesikkala <mikko.vesikkala@reaktor.fi>
2016-04-02 09:58:47 +0300

    Langlink

--- src/helpers/langlink.js
+++ src/helpers/langlink.js
@@ -1,14 +1,12 @@
 var Handlebars = require('handlebars')
 var find = require('lodash.find')
 
-module.exports = function ifNotLocale(locale, block, ctx) {
-  if(block.data.root.locale !== locale) {
-    var altFile = block.data.root.altFiles[locale]
-    if (locale === 'fi') {
-      var path = '/' + (altFile && altFile.slug ? altFile.slug + '/' : '')
-      return '<li><a rel="alternate" hreflang="fi" href="' + path + '">fi</a></li>'
-    }
-    var path = '/' + (altFile && altFile.locale ? altFile.locale + '/': '') + (altFile && altFile.slug ? altFile.slug + '/' : '')
-    return '<li><a rel="alternate" hreflang="' + locale + '" href="' + path + '">' + locale + '</a></li>'
+module.exports = function langlink(locale, block) {
+  var altFile = block.data.root.altFiles[locale]
+  if (locale === 'fi') {
+    var path = '/' + (altFile && altFile.slug ? altFile.slug + '/' : '')
+    return path
   }
+  var path = '/' + (altFile && altFile.locale ? altFile.locale + '/': '') + (altFile && altFile.slug ? altFile.slug + '/' : '')
+  return path
 }
